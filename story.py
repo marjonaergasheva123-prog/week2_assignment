981e4fc73c26fdf18fde2cd91154a7b793955b55
Marjona Ergasheva <marjonaergasheva123@gmail.com>
2025-10-05 13:19:01 +0500

    Add story.py, a simple mad libs style program

---
+++ story.py
@@ -0,0 +1,10 @@
+print("Let's write a story! Please provide some words.")
+
+# Get words from the user
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb_past_tense = input("Enter a verb (past tense): ")
+
+# Print the story
+print("\nHere is your story:")
+print("The " + adjective + " " + noun + " " + verb_past_tense + " over the lazy dog.")
